734685062b19fd6a0714d4ff16d853e19dba5894
FubyCutie <roset280905@gmail.com>
2022-04-16 23:56:00 +0100

    changed finish check to compare list contents rather than list equality

--- src/main/java/com/gmail/qwerty12944qwerty/pgjigsaw/main/Main.java
+++ src/main/java/com/gmail/qwerty12944qwerty/pgjigsaw/main/Main.java
@@ -96,7 +96,7 @@ public class Main  extends JavaPlugin implements Listener {
                     event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.LEVEL_UP, 1, 1);
                     event.getPlayer().sendMessage("§aYou completed this board in: §b"+Utils.ROUND.format(((float) Duration.between(Core.boardBegin, Instant.now()).toMillis() / 1000.0f))+"s");
                     Core.playersDone.add(event.getPlayer());
-                    if (Core.playersDone.equals(Core.playersPlaying)) {
+                    if (Core.playersDone.containsAll(Core.playersPlaying) && Core.playersPlaying.containsAll(Core.playersDone)) {
                         Core.currentScore++;
                         Core.end();
                     }
